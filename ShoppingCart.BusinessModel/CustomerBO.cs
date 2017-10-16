70039907c3f6e74e4a49f503b93495139e4df7d0
M <ramasubba.m@cgi.com>
2017-10-16 14:08:09 +0530

    oct16

---
+++ ShoppingCart.BusinessModel/CustomerBO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.BusinessModel
+{
+    public class CustomerBO : BaseBO
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public int RoleID { get; set; }
+
+    }
+}
